dbc0548daf08f3d38147585fefd77403372e30a1
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2022-10-19 22:49:06 +0530

    Add files via upload

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,17 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import connect from "./configs/db.js";
 import userController  from './controllers/user.controller.js'
+import cors from 'cors'
 const app=express();
-
+app.use(cors());
 app.use(express.json())
-
+dotenv.config()
 app.use('/users',userController)
-
-app.listen(4000,async (req,res)=>{
+const port=process.env.PORT ||4000
+app.listen(port,async (req,res)=>{
     try {
         await connect()
+        console.log(`listening to the port ${port}`)
     } catch (error) {
         console.log(error)
     }
-    console.log('listening to the port 5000')
+   
 })
